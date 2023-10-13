c821ad98bd914202e5a29e2aaad6f2936e9fa347
braydenwny <147541950+braydenwny@users.noreply.github.com>
2023-10-13 17:31:46 -0400

    Fixed bugs in js
    fixed bug where it wasn't finding the correct html objects (had to move script down in the html file)

--- script.js
+++ script.js
@@ -6,42 +6,50 @@ const questions = [
             {text: "compares adjacent elements and swaps them if they are incorrect", correct: false},
             {text: "repeatedly inserts elements into a sorted subarray", correct: false},
             {text: "repeatedly finds the smallest element and swaps it with the first element", correct: true},
-            {text: "recursively splits the array in half and merges them back together in sorted order", correct: false}
-        ],
+            {text: "recursively splits the array in half and merges them back together in sorted order", correct: false},
+        ]
+    },
+    {
         question: "Which describes bubble sort?",
-        answers: [
+            answers: [
             {text: "repeatedly inserts elements into a sorted subarray", correct: false},
             {text: "compares adjeacent elements and swaps them if they are incorrect", correct: true},
             {text: "recursively splits the array in half and merges them back together in sorted order", correct: false},
-            {text: "recursively partitions an array around a pivot element", correct: false}
-        ],
+            {text: "recursively partitions an array around a pivot element", correct: false},
+        ]
+    },
+    {
         question: "Which describles merge sort?",
         answers: [
             {text: "recursively partitions an array around a pivot element", correct: false},
             {text: "compares adjacent elements and swaprs  them if they are incorrect", correct: false},
             {text: "repeatedly inserts elements into a sorted subarray", correct: false},
-            {text: "recursively splits the array in half and merges them back together in sorted order", correct: true}
-        ],
+            {text: "recursively splits the array in half and merges them back together in sorted order", correct: true},
+        ]
+    },
+    {
         question: "Which describes insertion sort?",
         answers: [
             {text: "repeatedly inserts elements into a sorted subarray", correct: true},
             {text: "recursively splits the array in half and merges them back together in sorted order", correct: false},
             {text: "compares adjacent elements and swaps them if they are incorrect", correct: false},
-            {text: "repeatedly find the smallest element and swaps it with the first element", correct: false}
-        ],
+            {text: "repeatedly find the smallest element and swaps it with the first element", correct: false},
+        ]
+    },
+    {
         question: "Which describes quick sort?",
         answers: [
             {text: "recursively splits the array in half and merges them back together", correct: false},
             {text: "repeatedly finds the smallest element and swaps it with the first element", correct: false},
             {text: "recusively partitions an array around a pivot element", correct: true},
-            {text: "repeatedly inserts elements into a sorted subarray", correct: false}
+            {text: "repeatedly inserts elements into a sorted subarray", correct: false},
         ]
     }
-]
+];
 
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("nxtButton");
+const nextButton = document.getElementById("nextButton");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -61,7 +69,7 @@ function showQuestion(){
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");        //creates the button
-        button.innerHTML = currentQuestion.answer.text          //adds the text from the answer text
+        button.innerHTML = answer.text          //adds the text from the answer text
         button.classList.add("btn");                            //gives it the classname btn
         answerButton.appendChild(button);
     })
